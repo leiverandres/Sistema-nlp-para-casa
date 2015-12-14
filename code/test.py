2fa5c49f80b2d37740039be2ac7ad4a4d8d0eb43
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2015-12-14 03:21:45 +0000

    Testing

--- code/test.py
+++ code/test.py
@@ -7,7 +7,9 @@ st = "calentar por 5 min"
 # que nos ofrece Python para nombrar estos grupos y que sea mas facil identificarlos.
 
 m = re.compile(r"(.*)(?P<numero>[0-9]+)( min(utos)?)")
+# Formas de usarse:
 s = m.search(st)
+#s = m.match(st)
 minutos = (s.group("numero")) # es de tipo str pero se puede castear a entero
 
 print "Minutos: " + minutos
