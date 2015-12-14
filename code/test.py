e39dc1942fa58bac285dd38148a2d0a27662d68f
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2015-12-14 03:15:15 +0000

    Testing groups

---
+++ code/test.py
@@ -0,0 +1,13 @@
+import re
+
+st = "calentar por 5 min"
+
+# Referencias: http://relopezbriega.github.io/blog/2015/07/19/expresiones-regulares-con-python/
+# Podemos utilizar la sintaxis especial (?P<nombre>patron) 
+# que nos ofrece Python para nombrar estos grupos y que sea mas facil identificarlos.
+
+m = re.compile(r"(.*)(?P<numero>[0-9]+)( min(utos)?)")
+s = m.search(st)
+minutos = (s.group("numero")) # es de tipo str pero se puede castear a entero
+
+print "Minutos: " + minutos
